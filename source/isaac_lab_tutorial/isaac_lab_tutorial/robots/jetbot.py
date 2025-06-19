5dd76d452fa17380085afa5f04d9ec97f2431c56
mgussert <mgussert@nvidia.com>
2025-06-18 17:30:43 -0700

    updated jetbot path

--- source/isaac_lab_tutorial/isaac_lab_tutorial/robots/jetbot.py
+++ source/isaac_lab_tutorial/isaac_lab_tutorial/robots/jetbot.py
@@ -4,6 +4,6 @@ from isaaclab.actuators import ImplicitActuatorCfg
 from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
 
 JETBOT_CONFIG = ArticulationCfg(
-    spawn=sim_utils.UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Robots/Jetbot/jetbot.usd"),
+    spawn=sim_utils.UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Robots/NVIDIA/Jetbot/jetbot.usd"),
     actuators={"wheel_acts": ImplicitActuatorCfg(joint_names_expr=[".*"], damping=None, stiffness=None)},
 )
